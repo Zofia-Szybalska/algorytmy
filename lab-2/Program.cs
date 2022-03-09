a6f5a036f90359833221141d84c4a8897391d409
Zofia Szybalska <Zofia.Szybalska@microsoft.wsei.edu.pl>
2022-03-09 12:46:17 +0100

    Zad1

--- lab-2/Program.cs
+++ lab-2/Program.cs
@@ -6,7 +6,28 @@ namespace lab_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = silnia(5);
+            Console.WriteLine("KONIEC " + result);
+            int[] test1 = new int[] { 0,6,4,8,2,-1,5,7};
+            Console.WriteLine(zadanie1(test1,0,0));
+        }
+        static long silnia(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            Console.WriteLine("n = {0}", n);
+            return n * silnia(n-1);
+        }
+        static long zadanie1(int[] arr, int index, long sum)
+        {
+            if (index == arr.Length)
+            {
+                return sum;
+            }
+            sum += arr[index] + zadanie1(arr, index + 1, sum);
+            return sum;
         }
     }
 }
