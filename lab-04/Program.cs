6be97a146b92487aa7f73138bce14bcc34358a40
Zofia Szybalska <zochula111@gmail.com>
2022-03-23 18:41:26 +0100

    QuickSort Fixed

--- lab-04/Program.cs
+++ lab-04/Program.cs
@@ -1,39 +1,28 @@
 ﻿using System;
 
-namespace lab_04
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] test = new int[] { 2, 9, 8, 5, 6, 1 };
+namespace lab_04 {
+    class Program {
+        static void Main(string[] args) {
+            int[] test = new int[] { 7, 5, 6, 3, 4, 2, 1, 8, 9, 3 };
             QuickSort(test, test.Length);
-            foreach (var item in test)
-            {
+            foreach (var item in test) {
                 Console.Write(item + ", ");
             }
         }
 
-        static void QuickSort(int[] arr, int arrLength, int arrayBeg = 0)
-        {
+        static void QuickSort(int[] arr, int arrLength, int arrayBeg = 0) {
             if (arrLength <= 1) return;
             int elementOsiowy = arr[arrayBeg];
             int indexPrawy = -1;
-            int arrLengthCount = arrLength;
-            for (int i = arrayBeg; 0 < arrLengthCount; i++, arrLengthCount--)
-            {
-                if (arr[i] >= elementOsiowy)
-                {
-                    if (indexPrawy == -1)
-                    {
+            for (int i = arrayBeg + 1; i < arrayBeg + arrLength; i++) {
+                if (arr[i] >= elementOsiowy) {
+                    if (indexPrawy == -1) {
                         indexPrawy = i;
                     }
                 }
-                else if (arr[i] < elementOsiowy && indexPrawy != -1)
-                {
+                else if (arr[i] < elementOsiowy && indexPrawy != -1) {
                     int temp, curr = arr[i];
-                    for (int j = indexPrawy; j <= i; j++)
-                    {
+                    for (int j = indexPrawy; j <= i; j++) {
                         temp = arr[j];
                         arr[j] = curr;
                         curr = temp;
@@ -41,17 +30,15 @@ namespace lab_04
                     indexPrawy++;
                 }
             }
-            if (indexPrawy < 1)
-            {
+            if (indexPrawy < 1) {
                 QuickSort(arr, arrLength - 1, arrayBeg + 1);
             }
-            else
-            {
-                int temp2 = arr[0];
-                arr[0] = arr[indexPrawy - 1];
+            else {
+                int temp2 = arr[arrayBeg];
+                arr[arrayBeg] = arr[indexPrawy - 1];
                 arr[indexPrawy - 1] = temp2;
-                QuickSort(arr, indexPrawy, arrayBeg);
-                QuickSort(arr, arrLength - indexPrawy, indexPrawy - 1);
+                QuickSort(arr, indexPrawy - arrayBeg, arrayBeg);
+                QuickSort(arr, arrLength - indexPrawy, indexPrawy);
             }
         }
     }
