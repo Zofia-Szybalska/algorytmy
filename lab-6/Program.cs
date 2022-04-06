ee27032178ec476af8cf20f7b8034ca6591a8ce4
Zofia Szybalska <Zofia.Szybalska@microsoft.wsei.edu.pl>
2022-04-06 13:49:21 +0200

    PriorityQueue

--- lab-6/Program.cs
+++ lab-6/Program.cs
@@ -46,6 +46,98 @@ namespace lab_6
             return Count() == _arr.Length;
         }
     }
+    class PriorityQueue
+    {
+        public int[] _arr;
+        private int _last = -1;
+        public PriorityQueue(int size)
+        {
+            _arr = new int[size];
+        }
+        public bool Insert(int value)
+        {
+            if (IsFull())
+                return false;
+
+            _arr[++_last] = value;
+            RebuildUp(_last);
+            return true;
+        }
+        private void RebuildUp(int node)
+        {
+            while (node > 0)
+            {
+                int parentValue = _arr[Parent(node)];
+                int nodeValue = _arr[node];
+                if (nodeValue > parentValue)
+                {
+                    (_arr[Parent(node)], _arr[node]) = (_arr[node], _arr[Parent(node)]);
+                    node = Parent(node);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        public int Remove()
+        {
+            if (IsEmpty())
+                throw new Exception();
+
+            int removed = _arr[0];
+            _arr[0] = _arr[_last--];
+            RebuildDown();
+            return removed;
+        }
+        private void RebuildDown()
+        {
+            int node = 0;
+            while (node <= _last)
+            {
+                int leftChildValue = _arr[Left(node)];
+                int rightChildValue = _arr[Right(node)];
+                int nodeValue = _arr[node];
+                if (nodeValue >= leftChildValue && nodeValue >= rightChildValue)
+                    break;
+
+                if (leftChildValue >= rightChildValue)
+                {
+                    (_arr[node], _arr[Left(node)]) = (_arr[Left(node)], _arr[node]);
+                    node = Left(node);
+                }
+                else
+                {
+                    (_arr[node], _arr[Right(node)]) = (_arr[Right(node)], _arr[node]);
+                    node = Right(node);
+                }
+            }
+        }
+        public int Count()
+        {
+            return _last + 1;
+        }
+        public bool IsEmpty()
+        {
+            return Count() == 0;
+        }
+        public bool IsFull()
+        {
+            return Count() == _arr.Length;
+        }
+        private int Parent(int child)
+        {
+            return (child - 1) / 2;
+        }
+        private int Left(int parent)
+        {
+            return parent * 2 + 1;
+        }
+        private int Right(int parent)
+        {
+            return parent * 2 + 2;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -75,6 +167,17 @@ namespace lab_6
             }
             Console.WriteLine("IsFull : " + queue.IsFull());
             Console.WriteLine( queue.Remove() == 8);
+            PriorityQueue priority = new PriorityQueue(5);
+            Console.WriteLine("Kolejka prioretytowa");
+            priority.Insert(4);
+            priority.Insert(8);
+            priority.Insert(9);
+            priority.Insert(91);
+            Console.WriteLine(priority.Remove() == 91);
+            Console.WriteLine(priority.Remove() == 9);
+            Console.WriteLine(priority.Remove() == 8);
+            Console.WriteLine(priority.Remove() == 4);
+            Console.WriteLine(priority.Count() == 0);
         }
     }
 }
