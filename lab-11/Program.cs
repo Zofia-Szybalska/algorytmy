2054c6f761e95bf3259ba0b1dc28e408ff3afed0
Zofia Szybalska <zochula111@gmail.com>
2022-06-01 13:07:32 +0200

    lab 12

--- lab-11/Program.cs
+++ lab-11/Program.cs
@@ -155,28 +155,102 @@ namespace lab_11 {
                 }
                 return distances;
             }
+            public int[,] GetMatrix() {
+                int[,] matrix = new int[6, 6];
+                for (int i = 0; i < 6; i++) {
+                    foreach (Edge edge in Edges[i + 1]) {
+                        matrix[i, edge.Node - 1] = (int)edge.Weight;
+                    }
+                }
+                return matrix;
+            }
+
+            int minDistance(int[] distance,
+                        bool[] sptSet) {
+                int min = int.MaxValue, min_index = -1;
+                for (int v = 0; v < 6; v++)
+                    if (sptSet[v] == false && distance[v] <= min) {
+                        min = distance[v];
+                        min_index = v;
+                    }
+                return min_index;
+            }
+            public void GetDistance(int start) {
+                int[] distance = new int[6];
+                bool[] sptSet = new bool[6];
+                for (int i = 0; i < 6; i++) {
+                    distance[i] = int.MaxValue;
+                    sptSet[i] = false;
+                }
+                distance[start] = 0;
+                int[,] matrix = GetMatrix();
+                for (int count = 0; count < 6 - 1; count++) {
+
+                    int u = minDistance(distance, sptSet);
+                    sptSet[u] = true;
+                    for (int v = 0; v < 6; v++)
+                        if (!sptSet[v] && matrix[u, v] != 0 &&
+                             distance[u] != int.MaxValue && distance[u] + matrix[u, v] < distance[v])
+                            distance[v] = distance[u] + matrix[u, v];
+                }
+                Console.WriteLine("\nVertex    Distance from source");
+                for (int i = 0; i < 6; ++i)
+                    Console.WriteLine("{0}\t  {1}", i + 1, distance[i]);
+            }
+        
+        int minDistance(double[] distance, bool[] sptSet) {
+            double min = int.MaxValue;
+            int min_index = -1;
+            for (int v = 0; v < Edges.Count + 1; v++)
+                if (sptSet[v] == false && distance[v] <= min) {
+                    min = distance[v];
+                    min_index = v;
+                }
+            return min_index;
         }
-        static void Main(string[] args) {
-            /*var graph = new AdGraph(5);
-            graph.AddEdge(1, 4);
-            graph.AddEdge(3, 4);
-            graph.AddEdge(2, 4);
-            //Console.WriteLine(graph.ToString());
-            var graph2 = new AdGraph(3);
-            graph2.AddDirectedEdge(0, 1);
-            graph2.AddDirectedEdge(1, 2);
-            graph2.AddDirectedEdge(2, 1);
-            graph2.AddDirectedEdge(1, 0);
-            Console.WriteLine(graph2.ToString());*/
-            Graph graph = new Graph();
-            graph.AddDirectedEdge(1, 2, 1);
-            graph.AddDirectedEdge(1, 3, 2);
-            graph.AddDirectedEdge(1, 4, 3);
-            graph.AddDirectedEdge(2, 5, 4);
-            graph.AddDirectedEdge(5, 6, 5);
-            graph.AddDirectedEdge(3, 6, 6);
-            graph.AddDirectedEdge(4, 6, 7);
-            graph.BFTraversal(2, n => Console.Write(n + " "));
+        public void GetDistance2(int start) {
+            double[] distance = new double[Edges.Count + 1];
+            bool[] sptSet = new bool[Edges.Count + 1];
+            for (int i = 0; i < Edges.Count + 1; i++) {
+                distance[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+            distance[start] = 0;
+            for (int count = 0; count < Edges.Count; count++) {
+                int u = minDistance(distance, sptSet);
+                sptSet[u] = true;
+                for (int v = 0; v < Edges[u].Count; v++)
+                    if (!sptSet[v] && Edges[u][v].Node != 0 &&
+                         distance[u] != int.MaxValue && distance[u] + Edges[u][v].Weight < distance[Edges[u][v].Node])
+                        distance[Edges[u][v].Node] = distance[u] + Edges[u][v].Weight;
+            }
+            for (int i = 0; i < 6; i++) {
+                Console.WriteLine($"{distance[i]} - {i}");
+            }
         }
     }
+    static void Main(string[] args) {
+        /*var graph = new AdGraph(5);
+        graph.AddEdge(1, 4);
+        graph.AddEdge(3, 4);
+        graph.AddEdge(2, 4);
+        //Console.WriteLine(graph.ToString());
+        var graph2 = new AdGraph(3);
+        graph2.AddDirectedEdge(0, 1);
+        graph2.AddDirectedEdge(1, 2);
+        graph2.AddDirectedEdge(2, 1);
+        graph2.AddDirectedEdge(1, 0);
+        Console.WriteLine(graph2.ToString());*/
+        Graph graph = new Graph();
+        graph.AddDirectedEdge(1, 2, 1);
+        graph.AddDirectedEdge(1, 3, 2);
+        graph.AddDirectedEdge(1, 4, 3);
+        graph.AddDirectedEdge(2, 5, 4);
+        graph.AddDirectedEdge(5, 6, 5);
+        graph.AddDirectedEdge(3, 6, 6);
+        graph.AddDirectedEdge(4, 6, 7);
+        graph.BFTraversal(2, n => Console.Write(n + " "));
+        graph.GetDistance(1);
+    }
+}
 }
--- lab_12/Program.cs
+++ lab_12/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace lab_12
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if(Zadanie1("zakaz") == true && Zadanie1("robot") == false && Zadanie1("a") == true)
+namespace lab_12 {
+    class Program {
+        static void Main(string[] args) {
+            if (Zadanie1("zakaz") == true && Zadanie1("robot") == false && Zadanie1("a") == true)
                 Console.WriteLine("Zadanie1 poprawne");
             else
                 Console.WriteLine("Zadanie1 niepoprawne");
@@ -16,7 +14,7 @@ namespace lab_12
             else
                 Console.WriteLine("Zadanie2 niepoprawne");
 
-            if(Zadanie3("abc","cab") == true && Zadanie3("abc","caba") != true && Zadanie3("","") == true && Zadanie3("a","a") && Zadanie3("aabbcc","abcabc") && Zadanie3("abc", "adc") != true)
+            if (Zadanie3("abc", "cab") == true && Zadanie3("abc", "caba") != true && Zadanie3("", "") == true && Zadanie3("a", "a") && Zadanie3("aabbcc", "abcabc") && Zadanie3("abc", "adc") != true)
                 Console.WriteLine("Zadanie3 poprawne");
             else
                 Console.WriteLine("Zadanie3 niepoprawne");
@@ -24,13 +22,15 @@ namespace lab_12
                 Console.WriteLine("Zadanie4 poprawne");
             else
                 Console.WriteLine("Zadanie4 niepoprawne");
+            string test = Zadanie4("10000000000", "1111111111");
+            Console.WriteLine(Zadanie5("trzy jej działa jej", 3));
         }
 
         //czy łańcuch wejściowy jest palindromem
         //input zawsze ma conajmiej jeden znak
         public static bool Zadanie1(string input) {
-            for (int i = 0; i < input.Length/2; i++) {
-                if (input[i] != input[input.Length - i -1])
+            for (int i = 0; i < input.Length / 2; i++) {
+                if (input[i] != input[input.Length - i - 1])
                     return false;
             }
             return true;
@@ -38,20 +38,16 @@ namespace lab_12
 
         //Znajdź i zwróć pierwszy najdłuższy fragment złożony z jednakowych znaków
         //input ma conajmiej jeden znak
-        public static string Zadanie2(string input)
-        {
+        public static string Zadanie2(string input) {
             string result = "" + input[0];
             string max = "";
-            for (int i = 1; i < input.Length; i++)
-            {
+            for (int i = 1; i < input.Length; i++) {
                 char current = input[i];
                 char prev = input[i - 1];
-                if (prev == current)
-                {
+                if (prev == current) {
                     result += current;
                 }
-                else
-                {
+                else {
                     if (max.Length < result.Length)
                         max = result;
                     result = "" + current;
@@ -69,7 +65,7 @@ namespace lab_12
             var str2array = str2.ToCharArray();
             Array.Sort(str2array);
             for (int i = 0; i < str1array.Length; i++) {
-                if (str1array[i] != str2array[i] )
+                if (str1array[i] != str2array[i])
                     return false;
             }
             return true;
@@ -77,9 +73,25 @@ namespace lab_12
 
         //Dodawanie dwóch liczb całkowitych, dodatnich zapianych w łańcuchu
         //Dodawanie w słupku
-        public static string Zadanie4(string s1, string s2)
-        {
-            return "";
+        public static string Zadanie4(string s1, string s2) {
+            string result = "";
+            int reszta = 0;
+            if (s1.Length != s2.Length) {
+                int zeros = Math.Abs(s1.Length - s2.Length);
+                string zerosString = "";
+                for (int i = zeros; i > 0; i--) {
+                    zerosString += "0";
+                }
+                if (s1.Length < s2.Length) s1 = zerosString + s1;
+                else s2 = zerosString + s2;
+            }
+            for (int i = s1.Length - 1; i > -1; i--) {
+                int sum = (s1[i] - '0') + (s2[i]-'0') + reszta;
+                if (sum > 10) reszta = 1;
+                else reszta = 0;
+                result += sum;
+            }
+            return result;
         }
 
         //Fromatowanie do łańcuchów o długości nie większej od n
@@ -87,10 +99,13 @@ namespace lab_12
         // dla "abbcd edfg ijkl" n =5
         // abcd \nedfg \nijkl
         //bez łamania słów (dzielenia między wiersze) (jeśli chcesz ambitnie), wyświetlane w nowym wierszy, założenie n wieksze niż długość najdłuższego słowa
-        public static string Zadanie5(string input, int n)
-        {
+        public static string Zadanie5(string input, int n) {
 
-            return "";
+            if (input.Length < n) return input;
+            int j = 0;
+            for (int i = n; i < input.Length; i+=n+1) 
+                input =  input.Substring(0, i) + "\n" + input.Substring(i);
+            return input;
         }
     }
 }
